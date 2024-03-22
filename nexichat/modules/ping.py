7e8683dc6b0e565d003abcffc823ed12e41a5d23
ᴵ'ᵐ𝗖𝗢𝗗𝗘𝗥⋆─፝─™ <164161601+TEAM-CODERZZZ@users.noreply.github.com>
2024-03-22 08:41:35 -0700

    Update ping.py

--- nexichat/modules/ping.py
+++ nexichat/modules/ping.py
@@ -20,20 +20,20 @@ from nexichat.modules.helpers import PNG_BTN
 
 # Random Start Images
 IMG = [
-    "https://graph.org/file/210751796ff48991b86a3.jpg",
-    "https://graph.org/file/7b4924be4179f70abcf33.jpg",
-    "https://graph.org/file/f6d8e64246bddc26b4f66.jpg",
-    "https://graph.org/file/63d3ec1ca2c965d6ef210.jpg",
-    "https://graph.org/file/9f12dc2a668d40875deb5.jpg",
-    "https://graph.org/file/0f89cd8d55fd9bb5130e1.jpg",
-    "https://graph.org/file/e5eb7673737ada9679b47.jpg",
-    "https://graph.org/file/2e4dfe1fa5185c7ff1bfd.jpg",
-    "https://graph.org/file/36af423228372b8899f20.jpg",
-    "https://graph.org/file/c698fa9b221772c2a4f3a.jpg",
-    "https://graph.org/file/61b08f41855afd9bed0ab.jpg",
-    "https://graph.org/file/744b1a83aac76cb3779eb.jpg",
-    "https://graph.org/file/814cd9a25dd78480d0ce1.jpg",
-    "https://graph.org/file/e8b472bcfa6680f6c6a5d.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
+    "https://telegra.ph/file/a03ce0f6022a3b4ee7d80.jpg",
 ]
 
 
@@ -67,7 +67,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нey вαву!!\n{nexichat.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [MRDAXX❣️](https://t.me/{OWNER_USERNAME}) ||</b>",
+        text=f"𝐇𝐞𝐲 𝐁𝐚𝐛𝐲!!\n{nexichat.name} 𝐈𝐬 𝐀𝐥𝐢𝐯𝐞 🥀 𝐀𝐧𝐝 𝐖𝐨𝐫𝐤𝐢𝐧𝐠 𝐅𝐢𝐧𝐞 𝐖𝐢𝐭𝐡 𝐀 𝐏𝐢𝐧𝐠 𝐎𝐟\n➥ `{ms}` ms\n\n<b>|| 𝐌𝐚𝐝𝐞 𝐖𝐢𝐭𝐡  ❣️ 𝐁𝐲 [𝗜'𝗺 𝗖𝗢𝗗𝗘𝗥𝗭](https://t.me/{OWNER_USERNAME}) ||</b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
