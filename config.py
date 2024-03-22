ddbc27168900ac797e28f45036d7bfc35f4a7d24
ᴵ'ᵐ𝗖𝗢𝗗𝗘𝗥⋆─፝─™ <164161601+TEAM-CODERZZZ@users.noreply.github.com>
2024-03-22 08:49:18 -0700

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ API_HASH = "4e984ea35f854762dcde906dce426c2d"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = "6664582540"
+OWNER_ID = "7029942301"
 SUPPORT_GRP = "HEROKUFREECC"
 UPDATE_CHNL = "ALLTYPECC"
-OWNER_USERNAME = "iam_daxx"
+OWNER_USERNAME = "Noob_Coderzzz"
 
